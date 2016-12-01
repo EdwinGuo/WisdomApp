46ffce8f9e64be915886ddf88ba44547890b2975
EdwinGuo <zijing.guo@scotiabank.com>
2016-11-30 20:41:40 -0500

    initial commit for learning android

---
+++ app/src/main/java/guo/edwin/wisdomapp/MainActivity.java
@@ -0,0 +1,13 @@
+package guo.edwin.wisdomapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
