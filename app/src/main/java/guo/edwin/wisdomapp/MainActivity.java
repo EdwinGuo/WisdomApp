20264ed982f4ce1e9050db2a1aa1f11705adeae4
EdwinGuo <zijing.guo@scotiabank.com>
2016-12-07 09:08:29 -0500

    slidingingfragmentActivity day01-10

--- app/src/main/java/guo/edwin/wisdomapp/MainActivity.java
+++ app/src/main/java/guo/edwin/wisdomapp/MainActivity.java
@@ -4,11 +4,20 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setBehindContentView(R.layout.left_menu);
+
+        SlidingMenu slidmenu = getSlidingMenu();
+        slidmenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidmenu.setBehindOffset(850);
+
     }
 }
